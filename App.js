cf411679625269a39b338e1884e7a5e70a1a98f8
Maelys Guardini <louguardini@gmail.com>
2024-08-13 02:10:06 +0200

    login  screen reorder, sign in first - sign up after, redirect work, navigation update, profile creation on sign up, expo-camera implementation, UX rules, added expo list and multilist

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 import ProfilScreen from "./screens/ProfilScreen";
 import EventsScreen from "./screens/EventsScreen";
 import CalendarScreen from "./screens/CalendarScreen";
 import FriendsScreen from "./screens/FriendsScreen";
+import ProfileCreationScreen from "./screens/ProfileCreationScreen";
 
 import LoginScreen from "./screens/LoginScreen";
 
@@ -15,56 +16,49 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
-        let iconName = '';
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName = "";
 
-        if (route.name === 'Friends') {
-          iconName = 'users';
-        } 
-        
-        else if (route.name === 'Events') {
-          iconName = 'fort-awesome';
-        }
-         
-        else if (route.name === 'Calendar') {
-          iconName = 'calendar-plus-o';
-        }
+          if (route.name === "Friends") {
+            iconName = "users";
+          } else if (route.name === "Events") {
+            iconName = "fort-awesome";
+          } else if (route.name === "Calendar") {
+            iconName = "calendar-plus-o";
+          } else if (route.name === "Profil") {
+            iconName = "user";
+          } else if (route.name === "Test") {
+            iconName = "user";
+          }
 
-        else if (route.name === 'Profil') {
-          iconName = 'user';
-        }
-
-        else if (route.name === 'Test') {
-          iconName = 'user';
-        }
-
-        return <FontAwesome name={iconName} size={size} color={color} />;
-      },
-      tabBarActiveTintColor: '#ec6e5b',
-      tabBarInactiveTintColor: '#335561',
-      headerShown: false,
-    })}>
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#ec6e5b",
+        tabBarInactiveTintColor: "#335561",
+        headerShown: false,
+      })}
+    >
       <Tab.Screen name="Friends" component={FriendsScreen} />
       <Tab.Screen name="Events" component={EventsScreen} />
       <Tab.Screen name="Calendar" component={CalendarScreen} />
       <Tab.Screen name="Profil" component={ProfilScreen} />
-     
     </Tab.Navigator>
   );
 };
 
-
-
 export default function App() {
   return (
-   
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={LoginScreen} />
-          <Stack.Screen name="TabNavigator" component={TabNavigator} />
-        </Stack.Navigator>
-      </NavigationContainer>
-   
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={LoginScreen} />
+        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        <Stack.Screen
+          name="ProfileCreationScreen"
+          component={ProfileCreationScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -11,11 +11,13 @@ import { Image } from "react-native";
 
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import EventsScreen from "./EventsScreen";
+import ProfileCreationScreen from "./ProfileCreationScreen";
 
-const RegExpMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const RegExpMail =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 // const googlePng = () => {};
 const LoginScreen = ({ navigation }) => {
-  const [log, setLog] = useState(1);
+  const [log, setLog] = useState(2);
 
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
@@ -29,19 +31,26 @@ const LoginScreen = ({ navigation }) => {
 
   const signUp = () => {
     console.log(email, username, password);
+    navigation.navigate("ProfileCreationScreen");
   };
 
   return (
     <>
       {log === 1 ? (
         <SafeAreaView style={styles.container}>
-          <View style={{ flex: 1, alignItems: "center", marginTop: 40 }}>
-            <Text style={styles.logTitle}>Sign Up</Text>
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              marginTop: 50,
+            }}
+          >
+            <Text style={styles.logTitle}>Welcome to HangOut</Text>
 
             <View style={styles.inputContainer}>
               {/************************************Mail*************************************/}
               <View style={styles.inputSection}>
-                <Text style={styles.inputTitle}>Mail</Text>
+                {/* <Text style={styles.inputTitle}>Mail</Text> */}
                 <View style={styles.boxContainer}>
                   <FontAwesome style={styles.fontAwesome} name="envelope-o" />
                   <TextInput
@@ -57,7 +66,7 @@ const LoginScreen = ({ navigation }) => {
 
               {/****************************User-name****************************************/}
               <View style={styles.inputSection}>
-                <Text style={styles.inputTitle}>Username</Text>
+                {/* <Text style={styles.inputTitle}>Username</Text> */}
                 <View style={styles.boxContainer}>
                   <FontAwesome style={styles.fontAwesome} name="user" />
                   <TextInput
@@ -72,7 +81,7 @@ const LoginScreen = ({ navigation }) => {
 
               {/**********************************Password***********************************/}
               <View style={styles.inputSection}>
-                <Text style={styles.inputTitle}>Password</Text>
+                {/* <Text style={styles.inputTitle}>Password</Text> */}
                 <View style={styles.boxContainer}>
                   <FontAwesome style={styles.fontAwesome} name="lock" />
                   <TextInput
@@ -94,37 +103,53 @@ const LoginScreen = ({ navigation }) => {
                   Sign Up
                 </Text>
               </TouchableOpacity>
+              <TouchableOpacity onPress={() => setLog(2)}>
+                <Text
+                  style={{ marginTop: 30, color: "#9660DA", fontWeight: "600" }}
+                >
+                  Already have a account? Log in
+                </Text>
+              </TouchableOpacity>
 
-              <Text style={{ marginBottom: 20, marginTop: 20, color: "white" }}>
+              <Text
+                style={{ marginBottom: 20, marginTop: 20, color: "#9660DA" }}
+              >
                 OR
               </Text>
 
               {/**********************************Google***********************************/}
-              <TouchableOpacity style={styles.boxRadius} onPress={signUp}>
+
+              <TouchableOpacity style={styles.googleBoxRadius} onPress={signUp}>
                 <Image
                   style={styles.googleImg}
                   source={require("../assets/google.png")}
                 />
                 <Text style={styles.googleText}>Sign up with Google</Text>
               </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => setLog(2)}>
-                <Text style={{ marginTop: 60, color: "white" }}>
-                  Already have a account?
-                </Text>
-              </TouchableOpacity>
             </View>
           </View>
         </SafeAreaView>
       ) : (
         <SafeAreaView style={styles.container}>
-          <View style={{ flex: 1, alignItems: "center", marginTop: 40 }}>
-            <Text style={styles.logTitle}>Login</Text>
+          <View style={{ flex: 1, alignItems: "center", marginTop: 50 }}>
+            <Text style={styles.logTitle}>Welcome to HangOut</Text>
+
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: "600",
+                marginTop: 30,
+                marginBottom: 20,
+                color: "grey",
+              }}
+            >
+              Connect to my account
+            </Text>
 
             <View style={styles.inputContainer}>
               {/****************************User-name****************************************/}
               <View style={styles.inputSection}>
-                <Text style={styles.inputTitle}>Username</Text>
+                {/* <Text style={styles.inputTitle}>Username</Text> */}
                 <View style={styles.boxContainer}>
                   <FontAwesome style={styles.fontAwesome} name="user" />
                   <TextInput
@@ -136,10 +161,9 @@ const LoginScreen = ({ navigation }) => {
                   />
                 </View>
               </View>
-
               {/**********************************Password***********************************/}
               <View style={styles.inputSection}>
-                <Text style={styles.inputTitle}>Password</Text>
+                {/* <Text style={styles.inputTitle}>Password</Text> */}
                 <View style={styles.boxContainer}>
                   <FontAwesome style={styles.fontAwesome} name="lock" />
                   <TextInput
@@ -152,33 +176,47 @@ const LoginScreen = ({ navigation }) => {
                   />
                 </View>
               </View>
-
               <TouchableOpacity>
                 <Text
-                  style={{ marginBottom: 30, marginTop: 0, color: "white" }}
+                  style={{ marginBottom: 30, marginTop: -25, color: "#9660DA" }}
                 >
-                  Forgot password?
+                  Forgotten password?
                 </Text>
               </TouchableOpacity>
-
               {/**********************************Login***********************************/}
               <TouchableOpacity
                 style={styles.signup}
                 onPress={() => navigation.navigate("TabNavigator")}
               >
                 <Text
-                  style={{ color: "white", fontWeight: "bold", fontSize: 25 }}
+                  style={{ color: "white", fontWeight: "bold", fontSize: 20 }}
                 >
                   Login
                 </Text>
               </TouchableOpacity>
-
-              <Text style={{ marginBottom: 20, marginTop: 20, color: "white" }}>
+              <TouchableOpacity onPress={() => setLog(1)}>
+                <Text
+                  style={{ color: "#9660DA", fontWeight: "600", marginTop: 15 }}
+                >
+                  Don't have an account ? Sign up here.
+                </Text>
+              </TouchableOpacity>
+              <Text
+                style={{
+                  marginBottom: 20,
+                  marginTop: 60,
+                  color: "#9660DA",
+                  fontWeight: "800",
+                }}
+              >
                 OR
               </Text>
-
-              <TouchableOpacity onPress={() => setLog(1)}>
-                <Text style={{ color: "white" }}>SIGN UP</Text>
+              <TouchableOpacity style={styles.googleBoxRadius} onPress={signUp}>
+                <Image
+                  style={styles.googleImg}
+                  source={require("../assets/google.png")}
+                />
+                <Text style={styles.googleText}>Sign in with Google</Text>
               </TouchableOpacity>
             </View>
           </View>
@@ -191,30 +229,28 @@ const LoginScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#9660DA",
+    backgroundColor: "#fff",
     // alignItems: "center",
     // justifyContent: "center",
   },
   logTitle: {
-    fontSize: 40,
+    fontSize: 30,
     fontWeight: "bold",
-    color: "white",
+    color: "#9660DA",
     marginBottom: 20,
   },
   inputContainer: {
-    flex: 1,
     width: "100%",
-    padding: 5,
     alignItems: "center",
     justifyContent: "center",
   },
   inputSection: {
     width: "100%",
     alignItems: "center",
-    marginBottom: 40,
+    marginBottom: 30,
   },
   boxContainer: {
-    width: "100%",
+    width: "80%",
     flexDirection: "row",
     alignItems: "center",
     textAlign: "center",
@@ -223,11 +259,11 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     width: "100%",
-    height: 60,
+    height: 50,
     borderRadius: 50,
     borderWidth: 2,
     paddingHorizontal: 45,
-    borderColor: "white",
+    borderColor: "#9660DA",
     position: "relative",
   },
   fontAwesome: {
@@ -237,7 +273,8 @@ const styles = StyleSheet.create({
     marginRight: 15,
     marginBottom: 16,
     padding: 0,
-    color: "white",
+    paddingLeft: 5,
+    color: "#9660DA",
   },
   inputTitle: {
     position: "absolute",
@@ -247,17 +284,16 @@ const styles = StyleSheet.create({
     paddingLeft: 4,
     zIndex: 5,
     fontWeight: "bolder",
-    color: "white",
+    color: "#9660DA",
   },
 
   signup: {
     backgroundColor: "#9660DA",
     justifyContent: "center",
     alignItems: "center",
-    width: "100%",
-    height: 60,
+    width: "80%",
+    height: 50,
     borderRadius: 50,
-    borderWidth: 2,
     paddingHorizontal: 45,
     borderColor: "white",
     position: "relative",
@@ -265,12 +301,23 @@ const styles = StyleSheet.create({
   googleImg: {
     width: 30,
     height: 30,
-    marginRight: 50,
+    marginRight: 20,
   },
   googleText: {
     fontWeight: "bold",
     fontSize: 20,
-    color: "white",
+    color: "#9660DA",
+  },
+  googleBoxRadius: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "80%",
+    height: 50,
+    borderRadius: 50,
+    borderWidth: 2,
+    paddingHorizontal: 40,
+    borderColor: "#9660DA",
+    position: "relative",
   },
 
   // flex-row items-center w-full p-5 border-2 mb-10 border-purple-700 rounded-full
